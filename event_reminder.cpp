e114382dd1526f8281f9a3dbb562acbbddc64deb
Michael Francoo Fredy <michaelfrancoo4917@gmail.com>
2024-12-27 14:49:58 +0300

    Add headers and using namespace

---
+++ event_reminder.cpp
@@ -0,0 +1,7 @@
+// Include necessary libraries
+#include <iostream> // Input-output stream library
+#include <vector> // Vector library
+#include <string> // String library
+#include <ctime> // C time library
+// Use standard namespace
+using namespace std;
